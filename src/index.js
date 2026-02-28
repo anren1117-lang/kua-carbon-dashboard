983047201839e40946368a7daa56fa05573ed262
user <user@userdeMacBook-Pro.local>
2026-02-28 10:25:17 -0500

    Fix index.js with all admin routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import AdminPortal from './AdminPortal';
+import AdminFuel from './AdminFuel';
+import AdminStudents from './AdminStudents';
+import AdminTravel from './AdminTravel';
+import AdminWaste from './AdminWaste';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +16,10 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminPortal />} />
+        <Route path="/admin/fuel" element={<AdminFuel />} />
+        <Route path="/admin/students" element={<AdminStudents />} />
+        <Route path="/admin/travel" element={<AdminTravel />} />
+        <Route path="/admin/waste" element={<AdminWaste />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
